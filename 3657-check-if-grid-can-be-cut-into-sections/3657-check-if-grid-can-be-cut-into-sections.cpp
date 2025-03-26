ecf122a57d8fe570ed922c7595a3243653b4ec2b
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-03-26 10:20:58 +0530

    Time: 338 ms (36.1%), Space: 283.2 MB (35.43%) - LeetHub

--- 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
-    bool check(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
-        
-        int sections = 0;
-        int max_end = intervals[0][1];
-        
-        for (auto& interval : intervals) {
-            int start = interval[0], end = interval[1];
-            if (max_end <= start) {
-                sections++;
-            }
-            max_end = max(max_end, end);
-        }
-        
-        return sections >= 2;
-    }
-    
     bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
-        vector<vector<int>> x_intervals, y_intervals;
-        
-        for (auto& rect : rectangles) {
-            x_intervals.push_back({rect[0], rect[2]});
-            y_intervals.push_back({rect[1], rect[3]});
+
+        vector<vector<int>> hor;
+        vector<vector<int>> ver;
+        for(int i=0;i<rectangles.size();i++){
+            hor.push_back({min(rectangles[i][0],rectangles[i][2]),max(rectangles[i][0],rectangles[i][2])});
+            ver.push_back({min(rectangles[i][1],rectangles[i][3]),max(rectangles[i][1],rectangles[i][3])});
         }
-        
-        return check(x_intervals) || check(y_intervals);
+        sort(hor.begin(),hor.end());
+        sort(ver.begin(),ver.end());
+        bool ans=false;
+        int t=0;
+        int maxi=hor[0][1];
+        for(int i=1;i<hor.size();i++){
+            if(hor[i][0]>=maxi) t++;
+            maxi=max(maxi,hor[i][1]);
+
+        }
+        // cout<<t<<endl;
+        if(t>=2) ans=true;
+        maxi=ver[0][1];
+        t=0;
+         for(int i=1;i<hor.size();i++){
+            if(ver[i][0]>=maxi) t++;
+            maxi=max(maxi,ver[i][1]);
+        }
+        // cout<<t;
+        if(t>=2) ans=true;
+        return ans;
+
+
     }
 };
