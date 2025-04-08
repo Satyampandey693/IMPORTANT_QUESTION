937114aa0b2f639a2c080e19768cb431f834b725
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-04-08 13:28:13 +0530

    Time: 0 ms (100%), Space: 27.4 MB (85.32%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        map<int,int> mp;
+       vector<int> mp(101,0);
         int p=0;
         for(int i=nums.size()-1;i>=0;i--){
-            if(mp.find(nums[i])!=mp.end()){
+            if(mp[nums[i]]>0){
                 p=i+1;
                 break;
             }
