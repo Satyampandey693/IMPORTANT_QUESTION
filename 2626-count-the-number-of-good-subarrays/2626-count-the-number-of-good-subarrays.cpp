646c23b49106e06a9f7e88601b8a6eb7f18cd2c3
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-04-16 11:21:28 +0530

    Time: 244 ms (5.21%), Space: 81 MB (21.33%) - LeetHub

---
+++ 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.cpp
@@ -0,0 +1,44 @@
+
+#define ll long long
+ class Solution {
+public:
+ll pnc(ll p,ll q){
+    if(p<q) return 0;
+    ll ans=1;
+   for(int i=q;i>0;i--){
+            ans=ans*p;
+            p--;
+   }
+   while(q>0){
+    ans/=q;
+    q--;
+   }
+   return ans;
+}
+    long long countGood(vector<int>& nums, int k) {
+        long  long ans=0;
+        int i=0;
+        int j=0;
+        int n=nums.size();
+        map<int,int> mp;
+        int r=0;
+        while(i<n){
+            int t=mp[nums[i]];
+          mp[nums[i]]++;
+           r+=(int)pnc(mp[nums[i]],2)-(int)pnc(t,2);
+           while(r>=k&&j<i){
+            int l=mp[nums[j]];
+            mp[nums[j]]--;
+             r+=(int)pnc(mp[nums[j]],2)-(int)pnc(l,2);
+             ans+=(n-i);
+            //  cout<<i<<" ";
+             j++;
+
+           }
+           i++;
+           
+        }
+        return ans;
+
+    }
+};
