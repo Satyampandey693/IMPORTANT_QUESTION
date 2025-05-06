57439d5afc08a09d803cdc1a1d273f2ef8e59c51
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-05-06 11:36:51 +0530

    Time: 0 ms (100%), Space: 20.2 MB (96.05%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-       vector<int> ans(nums.size());
-       for(int i=0;i<nums.size();i++){
-        ans[i]=nums[nums[i]];
-       } 
-       return ans;
+        const int n=nums.size();
+        for(int i=0; i<n; i++){
+        // cout<<nums[nums[i]]<<" "<<(nums[nums[i]]&1023)<<endl;
+            nums[i]+=(nums[nums[i]]&1023)<<10;}
+        for(int i=0; i<n; i++)
+            nums[i]>>=10;
+        return nums;
     }
 };
