e3a5d1c3ff1c6f2e40086197d346ef668a614277
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-05-28 10:46:06 +0530

    Time: 0 ms (100%), Space: 126.7 MB (56.26%) - LeetHub

--- 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.cpp
@@ -1,28 +1,58 @@
 class Solution {
 public:
-    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-        int n = nums.size();
-        vector<vector<long long>> temp(n, vector<long long>(2, -1)); // temp[current_index(node)][is_even]
-        
-        return calculateMax(nums, n, k, 0, 1, temp);
+typedef long long ll;
+    long long maximumValueSum(vector<int>& nums, int k,
+                              vector<vector<int>>& edges) {
+        // vector<vector<long long>> memo(nums.size(), vector<long long>(2, -1));
+        // return maxSumOfNodes(0, 1, nums, k, memo);
+
+     ll idSum=0;
+     int c=0;
+     int minDec=INT_MAX;
+     int minInc=INT_MAX;
+     int n=nums.size();
+     for(int i=0;i<n;i++){
+     if((nums[i]^k)>=nums[i]){
+        idSum+=nums[i]^k;
+        // cout<<i<<" "<<(nums[i]^k)<<" "<<nums[i];
+        minInc=min(minInc,(nums[i]^k)-nums[i]);
+        c++;
+     }
+     else{
+        // cout<<i<<" "<<(nums[i]-(nums[i]^k))<<endl;
+        idSum+=nums[i];
+        minDec=min(minDec,nums[i]-(nums[i]^k));
+     }
+     }
+     if(c&1){
+        // cout<<minInc<<" "<<minDec;
+       if(minInc>=minDec)
+        return idSum-minDec;
+       return idSum-minInc;
+     }
+     else return idSum;
+
     }
 
 private:
-    // calculate_max -> cur_ind -> cur_index of the tree and is_even represents whether we have already changed (XOR) even or odd number of nodes 
-    long long calculateMax(vector<int>& nums, int n, int k, int curInd, int isEven, vector<vector<long long>>& temp) {
-        if (curInd == n) {  // if we go to node which doesn't exist
-            return isEven == 1 ? 0 : LLONG_MIN;
+    long long maxSumOfNodes(int index, int isEven, vector<int>& nums, int k,
+                            vector<vector<long long>>& memo) {
+        if (index == nums.size()) {
+            // If the operation is performed on an odd number of elements return
+            // INT_MIN
+            return isEven == 1 ? 0 : INT_MIN;
         }
-        if (temp[curInd][isEven] != -1) {  // if we've already encountered this state
-            return temp[curInd][isEven];
+        if (memo[index][isEven] != -1) {
+            return memo[index][isEven];
         }
+        // No operation performed on the element
+        long long noXorDone =
+            nums[index] + maxSumOfNodes(index + 1, isEven, nums, k, memo);
+        // XOR operation is performed on the element
+        long long xorDone = (nums[index] ^ k) +
+                            maxSumOfNodes(index + 1, isEven ^ 1, nums, k, memo);
 
-        // checking all possible variants (no XOR or XOR)
-        long long noXor = nums[curInd] + calculateMax(nums, n, k, curInd + 1, isEven, temp);  // we don't change the number of XOR nodes
-        long long withXor = (nums[curInd] ^ k) + calculateMax(nums, n, k, curInd + 1, !isEven, temp);  // we added 1 XORed node
-
-        long long mxPossible = max(noXor, withXor);
-        temp[curInd][isEven] = mxPossible;
-        return mxPossible;
+        // Memoize and return the result
+        return memo[index][isEven] = max(xorDone, noXorDone);
     }
 };
