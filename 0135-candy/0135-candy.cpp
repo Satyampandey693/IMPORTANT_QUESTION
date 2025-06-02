f7c9a641551f4639d637bec7ae3d62ae0367c068
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-06-02 10:11:25 +0530

    Time: 0 ms (100%), Space: 22.9 MB (59.49%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
+        // return 0;
         int n=ratings.size();
-        vector<int> can(n,1);
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        for(int i=0;i<n;i++)pq.push({ratings[i],i});
-        while(!pq.empty()){
-            int i=pq.top().second;
-            int cRat=pq.top().first;
-            if(i==0&&i==n-1) break;
-            if(i==n-1){
-                 if(ratings[i-1]<ratings[i]){
-                    can[i]=max(can[i],can[i-1]+1);
-                }
+        if(n==1) return 1;
+        vector<int> ans(ratings.size(),1);
+        for(int i=0;i<ratings.size();i++){
+            // if(i==0){
+            //     if(ratings[i]>ratings[i+1])
+            // }
+            if(i+1<n&&ratings[i]>ratings[i+1]){
+                ans[i]=ans[i+1]+1;
             }
-            else if(i==0){
-                if(ratings[i+1]<ratings[i]){
-                    can[i]=max(can[i],can[i+1]+1);
-                }
+            if(i>0&&ratings[i]>ratings[i-1]){
+                ans[i]=ans[i-1]+1;
             }
-            else{
-                if(ratings[i-1]<ratings[i]){
-                    can[i]=max(can[i],can[i-1]+1);
-                }
-                 if(ratings[i+1]<ratings[i]){
-                    can[i]=max(can[i],can[i+1]+1);
-                }
-            
-            }
-             pq.pop(); 
-
-
         }
-        int ans=accumulate(can.begin(),can.end(),0);
-        return ans;
+        
+        for(int i=ratings.size()-1;i>=0;i--){
+            // if(i==0){
+            //     if(ratings[i]>ratings[i+1])
+            // }
+            if(i+1<n&&ratings[i]>ratings[i+1]){
+                ans[i]=max(ans[i+1]+1,ans[i]);
+            }
+            if(i>0&&ratings[i]>ratings[i-1]){
+                ans[i]=max(ans[i],ans[i-1]+1);
+            }
+        }
+        int c=0;
+        for(int i=0;i<n;i++)c+=ans[i];
+        return c;
     }
 };
