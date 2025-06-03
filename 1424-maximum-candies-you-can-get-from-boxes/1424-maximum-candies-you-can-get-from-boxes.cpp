303bc0ccbbb2f6768e626ec2ba306cfee0214a67
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-06-03 14:30:44 +0530

    Time: 11 ms (30.77%), Space: 44.4 MB (60.58%) - LeetHub

--- 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
-        map<int,int> bx;
+       unordered_map<int,int> bx;
         // map<int,int> k;
         queue<int> q;
         int ans=0;
