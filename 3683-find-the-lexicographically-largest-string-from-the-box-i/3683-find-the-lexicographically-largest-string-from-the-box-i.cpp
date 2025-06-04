e029e1e507852027f500f0891938ebb066edd380
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-06-04 08:41:20 +0530

    Time: 14 ms (85.45%), Space: 36.6 MB (81.72%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     string answerString(string word, int numFriends) {
-        vector<int> v(26,-1);
+        // vector<int> v(26,-1);
         if(numFriends==1) return word;
-        for(int i=0;i<word.size();i++){
-            char c=word[i]-'a';
-            if(v[c]==-1){
-                v[c]=i;
-            }
-        }
+        // for(int i=0;i<word.size();i++){
+        //     char c=word[i]-'a';
+        //     if(v[c]==-1){
+        //         v[c]=i;
+        //     }
+        // }
 
-      int ind=-1;
-      char p;
-      for(int i=25;i>=0;i--){
-        if(v[i]!=-1&&ind==-1) ind=v[i];
-      }
-      p=word[ind];
-    //   int l1=ind;
-    //   cout<<l1<<endl;
+    //   int ind=-1;
+      char p= *max_element(word.begin(), word.end());;
+    //   for(int i=25;i>=0;i--){
+    //     if(v[i]!=-1&&ind==-1) ind=v[i];
+    //   }
+    //   p=word[ind];
       numFriends--;
-    //   if(l1>num)
-    //   numFriends-=l1;
     int l=word.size()-numFriends;
-      
-    //   numFriends=max(0,numFriends);
-    //   int l2=word.size()-ind;
-      
-    //   l2-=numFriends;
-    //   cout<<l2<<endl;
+    
     string ans;
     for(int i=0;i<word.size();i++){
         if(word[i]==p){
@@ -35,7 +26,6 @@ public:
             if(ans<q) ans=q;
         }
     }
-    //   string ans=word.substr(ind,l2);
       return ans;
     }
 };
