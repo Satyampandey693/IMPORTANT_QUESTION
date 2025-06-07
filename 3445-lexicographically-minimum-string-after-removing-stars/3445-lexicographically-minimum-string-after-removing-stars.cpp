f14176bc31a6c1c7bf39758055edd0bcefb1993b
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-06-07 15:23:49 +0530

    Time: 272 ms (22.4%), Space: 109.2 MB (16.56%) - LeetHub

--- 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     string clearStars(string s) {
-        vector<int> c(26,0);
-        vector<int> rm;
-        // priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+       
         map<int,vector<int>> mp;
         for(int i=0;i<s.size();i++){
            if(s[i]=='*'){
            
-                if(!mp.empty()){
                     int t=(*mp.begin()).first;
                     int r=(*mp.begin()).second.back();
                     (*mp.begin()).second.pop_back();
                     if((*mp.begin()).second.size()==0)mp.erase(t);
                     s[r]='-';
-                }
+              
            }
            else{
             mp[s[i]-'a'].push_back(i);
