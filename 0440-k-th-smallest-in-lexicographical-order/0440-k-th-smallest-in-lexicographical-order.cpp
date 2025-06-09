1ae2be1304e0f9564333de0a70445b8af629ed18
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-06-09 10:27:59 +0530

    Time: 0 ms (100%), Space: 7.9 MB (9.23%) - LeetHub

---
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+ int check(long a, long b, long n) {
+        int gap = 0;
+        while (a <= n) {
+            gap += min(n + 1, b) - a;
+            a *= 10;
+            b *= 10;
+        }
+        return gap;
+    }
+
+//  void   find(int n,int r,int k){
+//         if(r>n) return ;
+        
+//     }
+    int findKthNumber(int n, int k) {
+          long num = 1;
+        for (int i = 1; i < k;) {
+            int req = check(num, num + 1, n);
+            if (i + req <= k) {
+                i += req;
+                num++;
+            } else {
+                i++;
+                num *= 10;
+            }
+        }
+        return num;
+    }
+};
