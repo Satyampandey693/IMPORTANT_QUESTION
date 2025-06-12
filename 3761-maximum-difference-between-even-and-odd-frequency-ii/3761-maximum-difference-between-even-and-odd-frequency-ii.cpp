c291b0c33bf211918e6488665d5d78a438e19f62
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-06-12 07:53:30 +0530

    Time: 109 ms (54.05%), Space: 72.8 MB (51.35%) - LeetHub

--- 3761-maximum-difference-between-even-and-odd-frequency-ii/3761-maximum-difference-between-even-and-odd-frequency-ii.cpp
+++ 3761-maximum-difference-between-even-and-odd-frequency-ii/3761-maximum-difference-between-even-and-odd-frequency-ii.cpp
@@ -1,59 +1,55 @@
-class Solution 
-{
+class Solution {
 public:
-    int maxDifference(string s, int k) 
-    {
-        int n = s.size();
-        int ans = INT_MIN;
-
-        // Step 1: Try all (a, b) pairs
-        for (int a = 0; a <= 4; ++a) 
-        {
-            for (int b = 0; b <= 4; ++b) 
-            {
-                if (a == b)
+   int getStatus(int cnt_a, int cnt_b) {
+        return ((cnt_a & 1) << 1) | (cnt_b & 1);
+    }
+    int maxDifference(string s, int k) {
+        int n=s.size();
+        int ans=INT_MIN;
+        for(int i=0;i<5;i++){
+            for(char j=0;j<5;j++){
+                if(i==j) continue;
+                // if(i!=0||j!=1) continue;
+                // int f=0;
+                // int s=0;
+                vector<int> f(n+1,0);
+                vector<int> se(n+1,0);
+                // int par[2][2]={{INT_MAX,}}
+                vector<int> par={INT_MAX,INT_MAX,INT_MAX,INT_MAX};
+                 for (int k = 1; k <= n; ++k) 
                 {
-                    continue;
-                } 
-
-                vector<int> s1(n + 1);
-                vector<int> s2(n + 1);
-                
-               
-                for (int i = 1; i <= n; ++i) 
-                {
-                    s1[i] = s1[i - 1] + (s[i - 1] - '0' == a);
-                    s2[i] = s2[i - 1] + (s[i - 1] - '0' == b);
+                    f[k] = f[k - 1] + (s[k - 1] - '0' == i);
+                    se[k] = se[k - 1] + (s[k - 1] - '0' == j);
                 }
-
-               
-                int g[2][2] = {{INT_MIN, INT_MIN}, {INT_MIN, INT_MIN}};
-                int j = 0;
-
-                
-                for (int i = k; i <= n; ++i) 
-                {
-                    while (i - j >= k && s1[i] > s1[j] && s2[i] > s2[j]) 
-                    {
-                        int pa = s1[j] % 2;
-                        int pb = s2[j] % 2;
-                        g[pa][pb] = max(g[pa][pb], s2[j] - s1[j]);
-                        ++j;
-                    }
-
+                int p=0,q=1;
+                while(q<=n){
                    
-                    int pa = s1[i] % 2;
-                    int pb = s2[i] % 2;
-                    int need = g[1 - pa][pb];
-                    
-                    if (need != INT_MIN) 
-                    {
-                        ans = max(ans, (s1[i] - s2[i]) + need);
+                    //  int t=0;
+                    // if(!(f[q]%2)){
+                    //   t=2;
+                    // }
+                    // t+=(se[q]%2);
+                    while(q-p>=k && se[q]-se[p]>=2){
+                    //      int r=0;
+                    // if((f[p]%2)){
+                    //   r=2;
+                    // }
+                    // r+=(se[p]%2);
+                    int r=getStatus(f[p],se[p]);
+                    par[r]=min(par[r],f[p]-se[p]);
+                    p++;
                     }
+                     int right_status = getStatus(f[q],se[q]);
+                    int t = right_status ^ 2;
+                if(par[t]!=INT_MAX){
+                    ans=max(ans,(f[q]-se[q]-par[t]));
                 }
+                q++;
+                }
+                // cout<<endl;
+
             }
         }
-
-        return ans == INT_MIN ? -1 : ans;
+        return ans;
     }
 };
