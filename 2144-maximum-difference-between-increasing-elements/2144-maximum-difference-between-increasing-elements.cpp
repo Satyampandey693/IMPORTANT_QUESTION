7170df1c6651861a58c6942e092a6b273e3faa1f
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-06-16 08:50:56 +0530

    Time: 0 ms (100%), Space: 12 MB (38.11%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
@@ -4,11 +4,11 @@ public:
         int mx=-1;
         int ans=-1;
         for(int i=nums.size()-1;i>=0;i--){
-              if(mx!=-1){
+            //   if(mx!=-1){
                 ans=max(mx-nums[i],ans);
-              }
+            //   }
               mx=max(mx,nums[i]);
-              cout<<mx<<endl;
+            //   cout<<mx<<endl;
         }
         if(ans==0) return -1;
         return ans;
