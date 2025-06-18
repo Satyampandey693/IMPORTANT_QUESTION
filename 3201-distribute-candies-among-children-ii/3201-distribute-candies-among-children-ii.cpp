97c2b994248f86ad00644cf83c4881eba9d506f9
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-06-18 15:02:39 +0530

    Time: 2 ms (76.48%), Space: 8.8 MB (97.37%) - LeetHub

--- 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     long long H3(long long n){
-        return n<0?0:(n+1)*(n+2)/2;
+        // if(n<0) return 0;
+        if(n<=1) return 0;
+        return n<0?0:(n)*(n-1)/2;
     }
     long long distributeCandies(int n, int limit) {
         // return H3(n)-3LL*H3(n-limit-1)+3LL*H3(n-2*(limit+1))-H3(n-3*(limit+1));
-        long long ans=0;
-        for(int x=0;x<=limit;x++){
-            int s=n-x;//y+z
-            if(s>2*limit||s<0) continue;//if y+z>2*l then its not good sol
-            int ymax=min(s,limit);//when z=0 hoga then x min hoga
-            int ymin=max(0,s-limit);//when z max value hoga then y min hoga
-            // cout<<ymax-ymin+1<<endl;
-            ans+=(ymax-ymin+1);
-        }
-        return ans;
+        // cout<<
+       return  H3(n+2)+3*H3(n-2*limit)-3*H3(n-limit+1)-H3(n-3*limit-1);
+        // return max(p,(long long)0);
+        // long long ans=0;
+        // for(int x=0;x<=limit;x++){
+        //     int s=n-x;//y+z
+        //     if(s>2*limit||s<0) continue;//if y+z>2*l then its not good sol
+        //     int ymax=min(s,limit);//when z=0 hoga then x min hoga
+        //     int ymin=max(0,s-limit);//when z max value hoga then y min hoga
+        //     // cout<<ymax-ymin+1<<endl;
+        //     ans+=(ymax-ymin+1);
+        // }
+        // return ans;
     }
 };
