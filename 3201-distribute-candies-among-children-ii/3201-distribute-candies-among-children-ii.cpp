333678da67b905861f5a4b3c297db1ecae30058d
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-06-18 11:13:00 +0530

    Time: 28 ms (43.45%), Space: 9 MB (17.54%) - LeetHub

--- 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
@@ -4,6 +4,16 @@ public:
         return n<0?0:(n+1)*(n+2)/2;
     }
     long long distributeCandies(int n, int limit) {
-        return H3(n)-3LL*H3(n-limit-1)+3LL*H3(n-2*(limit+1))-H3(n-3*(limit+1));
+        // return H3(n)-3LL*H3(n-limit-1)+3LL*H3(n-2*(limit+1))-H3(n-3*(limit+1));
+        long long ans=0;
+        for(int x=0;x<=limit;x++){
+            int s=n-x;//y+z
+            if(s>2*limit||s<0) continue;//if y+z>2*l then its not good sol
+            int ymax=min(s,limit);//when z=0 hoga then x min hoga
+            int ymin=max(0,s-limit);//when z max value hoga then y min hoga
+            // cout<<ymax-ymin+1<<endl;
+            ans+=(ymax-ymin+1);
+        }
+        return ans;
     }
 };
