d1061cdd115d849739b3896b4078d95dd0356c8e
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-06-19 08:51:08 +0530

    Time: 14 ms (96.08%), Space: 90.9 MB (5.15%) - LeetHub

--- 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.cpp
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     int partitionArray(vector<int>& nums, int k) {
+        // int c=0;
+        // sort(nums.begin(),nums.end());
+        int mx=*max_element(nums.begin(),nums.end());
+        vector<int> arr(mx+1,0);
+         for(int i=0;i<nums.size();i++){
+               arr[nums[i]]++;
+        } 
         int c=0;
-        sort(nums.begin(),nums.end());
-        // int mx=0;
-        int j=0;
-        int i=0;
-        for(;i<nums.size();i++){
-                if(nums[i]-nums[j]<=k) continue;
-                c++;
-                j=i;
+        int mn=INT_MAX;
+        for(int i=0;i<mx+1;i++){
+            while(arr[i]==0){
+                i++;
+                continue;
+            }
+              mn=min(mn,i);
+              if(i-mn<=k)continue;
+              c++;
+              mn=i;
         }
+        // int mx=0;
+        // int j=0;
+        // int i=0;
+        // for(;i<nums.size();i++){
+        //         if(nums[i]-nums[j]<=k) continue;
+        //         c++;
+        //         j=i;
+        // }
         c++;
         return c;
     }
