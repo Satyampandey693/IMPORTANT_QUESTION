4e9839b07ac51284a3b881118a2bf737dd7e80d4
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-06-21 10:55:15 +0530

    Time: 816 ms (30.06%), Space: 467 MB (45.07%) - LeetHub

--- 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
@@ -1,25 +1,31 @@
-const int mod = 1e9 + 7;
-int mod_add(int a, int b) {a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
-
 class Solution {
 public:
+int mod=1e9+7;
     int lengthAfterTransformations(string s, int t) {
-        int nums[26] = {0};
-        for (char ch : s) nums[ch - 'a']++;
-        while (t--) {
-            int cur[26] = {0};
-            for (int j = 0; j < 26; j++) {
-                if (j == 25 && nums[j] > 0) {
-                    cur[0] = mod_add(cur[0], nums[j]);
-                    cur[1] = mod_add(cur[1], nums[j]);
-                } else {
-                    if (j < 25) cur[j + 1] = mod_add(cur[j + 1], nums[j]);
+        vector<long long> v(26,0);
+       for(int i=0;i<s.size();i++){
+          v[s[i]-'a']++;
+       }
+
+       for(int i=1;i<=t;i++){
+        vector<long long> nv(26,0);
+          for(int j=0;j<26;j++){
+                if(j==0){
+                    nv[j]=v[25];
                 }
-            }
-            for (int j = 0; j < 26; j++) nums[j] = cur[j];
-        }
-        int ans = 0;
-        for (int i : nums) ans = mod_add(ans, i);
-        return (int)ans;
+                else if(j==1){
+                    nv[j]=(v[25]+v[j-1])%mod;
+                }
+                else {
+                    nv[j]=v[j-1];
+                }
+          }
+          v=nv;
+       }
+       int ans=0;
+       for(int i=0;i<26;i++){
+        ans=(ans+v[i])%mod;
+       }
+       return ans;
     }
 };
