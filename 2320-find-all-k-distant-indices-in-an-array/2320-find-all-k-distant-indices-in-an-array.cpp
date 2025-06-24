75d67254bebf18e96cf405029b6b6e506a58f94e
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-06-24 07:37:49 +0530

    Time: 0 ms (100%), Space: 14.8 MB (55.05%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.cpp
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.cpp
@@ -3,24 +3,32 @@ public:
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
         int n=nums.size();
         vector<int> v(nums.size()+1,0);
+        int j=0;
+          vector<int> ans;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=key) continue;
             int ind=max(0,i-k);
-            v[ind]++; 
-            v[i+1]--;
+            // v[ind]++; 
+            // v[i+1]--;
+            j=max(j,ind);
             ind=min(n-1,i+k);
-            v[i]++;
-            v[ind+1]--;
+            // v[i]++;
+            // v[ind+1]--;
+            
+            while(j<=ind){
+               ans.push_back(j);
+               j++;  
+            }
         }
-        vector<int> ans;
+      
         // bool p=false;
-        if(v[0])ans.push_back(0);
-       for(int i=1;i<n;i++){
-        cout<<v[i]<<" ";
-        v[i]+=v[i-1];
+    //     if(v[0])ans.push_back(0);
+    //    for(int i=1;i<n;i++){
+    //     cout<<v[i]<<" ";
+    //     v[i]+=v[i-1];
         
-        if(v[i]) ans.push_back(i);
-       }
+    //     if(v[i]) ans.push_back(i);
+    //    }
      
        return ans;
 
