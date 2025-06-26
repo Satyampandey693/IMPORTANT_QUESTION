211b4484146bc53eda3991eb25cc840c2fe47f24
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-06-26 13:18:23 +0530

    Time: 0 ms (100%), Space: 9.5 MB (50.79%) - LeetHub

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,20 +1,71 @@
 class Solution {
 public:
+int pow(int a,int b){
+    if(b==0) return 1;
+    int pr=1;
+    while(b){
+      if(b&1){
+        pr=pr*a;
+        b--;
+      }
+      else{
+            a=a*a;
+            b/=2;
+      }
+    }
+    return pr;
+}
     int longestSubsequence(string s, int k) {
-        int sm = 0;
-        int cnt = 0;
-        int bits = 32 - __builtin_clz(k);
-        for (int i = 0; i < s.size(); ++i) {
-            char ch = s[s.size() - 1 - i];
-            if (ch == '1') {
-                if (i < bits && sm + (1 << i) <= k) {
-                    sm += 1 << i;
-                    cnt++;
-                }
-            } else {
-                cnt++;
-            }
+    //    int c=__builtin_popcount(k);
+    //    return c; 
+    int sz=0;
+    bool pr=false;
+    long long p=0;
+    int ind=0;
+    int c=0;
+    // cout<<s.size();
+    // cout<<pow(2,32);
+    for(int i=s.size()-1;i>=0;i--){
+        // if(k&1){
+        //     if(s[i]=='0'){
+        //          sz++;
+        //          k=k>>1;
+        //          c--;
+        //     }
+        //     else{
+        //       if(pr){
+        //         if(c>1){
+        //             sz++;
+        //             k=k>>1;
+        //             c--;
+
+        //         }
+        //         else{
+        //             c=0;
+                    
+        //         }
+        //       }
+        //     }
+        // }
+        if(s[i]=='0'){
+            sz++;
+            c++;
         }
-        return cnt;
+        else{
+            // cout<<"j";
+            if(ind<=30){
+                 p+=pow(2,ind);
+                  if(p<=k)sz++;
+            }
+           
+
+           
+        }
+        ind++;
+
+    }
+    // cout<<c<<endl;
+    return sz++;
+
     }
 };
