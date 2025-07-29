160b0d61a00f62d73e988e5b3f82410681a29dab
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-07-29 18:56:00 +0530

    Time: 34 ms (37.83%), Space: 44.3 MB (22.29%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -13,7 +13,17 @@ int find(string s,string t,int i,int j,  vector<vector<int>> &dp){
 
 }
     int numDistinct(string s, string t) {
-        vector<vector<int>> dp(s.size(),vector<int>(t.size(),-1));
-        return find(s,t,0,0,dp);
+         vector<vector<uint64_t>> dp(s.size()+1,vector<uint64_t> (t.size()+1,0));
+    for(int i = 0; i <= s.size(); i++) dp[i][0] = 1;
+    for(int i = 1; i <= s.size(); i++) {
+        for(int j = 1; j <= t.size(); j++) {
+            if(s[i-1] == t[j-1]) {
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+            } else {
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+    return (int)dp[s.size()][t.size()];
     }
 };
