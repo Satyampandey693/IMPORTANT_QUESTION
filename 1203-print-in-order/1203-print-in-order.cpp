ebf4686bd6ea233431e9e4112cbb8dd1e17b466d
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-07-30 09:12:42 +0530

    Time: 13 ms (19.17%), Space: 9.5 MB (78.31%) - LeetHub

---
+++ 1203-print-in-order/1203-print-in-order.cpp
@@ -0,0 +1,41 @@
+class Foo {
+public:
+bool p1=false,p2=false;
+condition_variable cv1,cv2;
+mutex mtx;
+    Foo() {
+        
+    }
+
+    void first(function<void()> printFirst) {
+        
+        // printFirst() outputs "first". Do not change or remove this line.
+       
+       
+        
+        printFirst();
+        p1=true;
+         cv1.notify_one();
+    }
+
+    void second(function<void()> printSecond) {
+        
+        // printSecond() outputs "second". Do not change or remove this line.
+        unique_lock<mutex> lock(mtx);
+        cv1.wait(lock, [this] { return p1; }); 
+        printSecond();
+        p2=true;
+        cv2.notify_one();
+    }
+
+    void third(function<void()> printThird) {
+        
+        // printThird() outputs "third". Do not change or remove this line.
+        // this_thread::sleep_for(chrono::milliseconds(3));
+    //    lock_guard<mutex> lock(m);
+     unique_lock<mutex> lock(mtx);
+        cv2.wait(lock, [this] { return p2; }); 
+    // cv2.wait(p2);
+        printThird();
+    }
+};
