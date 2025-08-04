5e0564004c6902e2694f53ecb81db72ea3e4fcd5
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-08-04 16:42:13 +0530

    Time: 22 ms (64.67%), Space: 139.3 MB (35.83%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -6,24 +6,15 @@ public:
         int j=0;
         vector<int> ans;
         for(int i=0;i<nums.size();i++){
-            // mp[nums[i]]++;
-            // while(!p.empty()&&p.front()<i-k+1) p.pop_front();
-           while (!p.empty() && nums[p.back()] <= nums[i]) {
-          p.pop_back();
-            }
-
-            p.push_back(i);
-            while(!p.empty()&&p.front()<i-k+1) p.pop_front();
-            // if(i>=k){
-            //   mp[nums[j]]--;
-            //   if( mp[nums[j]]==0) mp.erase(nums[j]);
-            //   j++;
-            // //   if(mp.find(nums[j])==mp])
-            // }
-           if(i>=k-1){
-            int mx=nums[p.front()];
-            ans.push_back(mx);}
+           while(!p.empty()&&nums[p.back()]<nums[i]){
+             p.pop_back();
+           }
+           p.push_back(i);
+           while(!p.empty()&&p.front()<=i-k) p.pop_front();
+           if(i>=k-1)
+           ans.push_back(nums[p.front()]);
         }
+
         return ans;
     }
 };
