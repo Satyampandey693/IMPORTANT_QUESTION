c92974f51f834ceca5507390ee16577b5f026498
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-08-06 17:12:28 +0530

    Time: 183 ms (68.41%), Space: 115.6 MB (60.45%) - LeetHub

---
+++ 0854-making-a-large-island/0854-making-a-large-island.cpp
@@ -0,0 +1,170 @@
+class Solution {
+public:
+class DisjointSet {
+    
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+// class Solution {
+// private:
+//     bool isValid(int adjr, int adjc, int n, int m) {
+//         return adjr >= 0 && adjr < n && adjc >= 0 && adjc < m;
+//     }
+// public:
+//     vector<int> numOfIslands(int n, int m,
+//                              vector<vector<int>> &operators) {
+//         DisjointSet ds(n * m);
+//         int vis[n][m];
+//         memset(vis, 0, sizeof vis);
+//         int cnt = 0;
+//         vector<int> ans;
+//         for (auto it : operators) {
+//             int row = it[0];
+//             int col = it[1];
+//             if (vis[row][col] == 1) {
+//                 ans.push_back(cnt);
+//                 continue;
+//             }
+//             vis[row][col] = 1;
+//             cnt++;
+//             // row - 1, col
+//             // row , col + 1
+//             // row + 1, col
+//             // row, col - 1;
+//             int dr[] = { -1, 0, 1, 0};
+//             int dc[] = {0, 1, 0, -1};
+//             for (int ind = 0; ind < 4; ind++) {
+//                 int adjr = row + dr[ind];
+//                 int adjc = col + dc[ind];
+//                 if (isValid(adjr, adjc, n, m)) {
+//                     if (vis[adjr][adjc] == 1) {
+//                         int nodeNo = row * m + col;
+//                         int adjNodeNo = adjr * m + adjc;
+//                         // if (ds.findUPar(nodeNo) != ds.findUPar(adjNodeNo)) {
+//                         //     cnt--;
+//                         //     ds.unionBySize(nodeNo, adjNodeNo);
+//                         // }
+
+//                     }
+//                 }
+//             }
+//             ans.push_back(cnt);
+//         }
+//         return ans;
+//     }
+// };
+ bool isValid(int adjr, int adjc, int n, int m) {
+        return adjr >= 0 && adjr < n && adjc >= 0 && adjc < m;
+    }
+    int largestIsland(vector<vector<int>>& grid) {
+        int n=grid.size();
+         int ans=0;
+        int m=grid[0].size();
+        DisjointSet ds(m*n);
+        for(int i=0;i<grid.size();i++){
+      for(int j=0;j<grid[i].size();j++){
+        if(grid[i][j]==0) continue;
+        // ans=1;
+        ans=max(ans,1);
+         int dr[] = { -1, 0, 1, 0};
+            int dc[] = {0, 1, 0, -1};
+            for (int ind = 0; ind < 4; ind++) {
+                int adjr = i + dr[ind];
+                int adjc = j+ dc[ind];
+                if (isValid(adjr, adjc, n, m)) {
+                    if (grid[adjr][adjc] == 1) {
+                        int nodeNo = i * m + j;
+                        int adjNodeNo = adjr * m + adjc;
+                        if (ds.findUPar(nodeNo) != ds.findUPar(adjNodeNo)) {
+                            // cnt--;
+                            ds.unionBySize(nodeNo, adjNodeNo);
+                            int count=ds.size[ds.findUPar(nodeNo)];
+                            ans=max(ans,count);
+                        }
+
+                    }
+                }
+            }
+      }
+        }
+       
+       for(int i=0;i<grid.size();i++){
+      for(int j=0;j<grid[i].size();j++){
+        if(grid[i][j]==1) continue;
+         int dr[] = { -1, 0, 1, 0};
+            int dc[] = {0, 1, 0, -1};
+            set<int> upr;
+            for (int ind = 0; ind < 4; ind++) {
+                int adjr = i+ dr[ind];
+                int adjc = j+ dc[ind];
+
+                if (isValid(adjr, adjc, n, m)) {
+                    if (grid[adjr][adjc] == 1) {
+                        int nodeNo = i * m + j;
+                        int adjNodeNo = adjr * m + adjc;
+                        // if (ds.findUPar(nodeNo) != ds.findUPar(adjNodeNo)) {
+                        //     cnt--;
+                        //     ds.unionBySize(nodeNo, adjNodeNo);
+                        // }
+                        int u=ds.findUPar(adjNodeNo);
+                        upr.insert(u);
+
+                    }
+                }
+            }
+            int sz=0;
+            for(auto it=upr.begin();it!=upr.end();it++){
+                sz+=ds.size[*it];
+            }
+            ans=max(ans,sz+1);
+      }
+        }
+        return ans;
+
+    }
+};
