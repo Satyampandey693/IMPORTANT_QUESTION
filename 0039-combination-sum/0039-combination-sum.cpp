0fc0fe3da7f5d167a55ad8701060a583b26465f4
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-08-06 10:37:27 +0530

    Time: 3 ms (55.84%), Space: 14.3 MB (46.07%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -2,22 +2,31 @@ class Solution {
 public:
 vector<vector<int>> ans;
 void find(int i,vector<int> &v,vector<int>& candidates, int target){
-    if(i>=candidates.size()) return;
-    if(target<0) return ;
-    if(target==0) {
+     if(target==0) {
         ans.push_back(v);
         return ;
     }
-    //not pick
-     find(i+1,v,candidates,target);
-    //pick
-    v.push_back(candidates[i]);
-    find(i,v,candidates,target-candidates[i]);
-    v.pop_back();
+    if(i>=candidates.size()) return;
+    if(target<0) return ;
+   
+    // //not pick
+    //  find(i+1,v,candidates,target);
+    // //pick
+    // v.push_back(candidates[i]);
+    // find(i,v,candidates,target-candidates[i]);
+    // v.pop_back();
+    //
+    //  find(i+1,v,candidates,target);
+    for(int j=i;j<candidates.size();j++){
+        // if(j>i&&candidates[j]==candidates[j-1]) continue;
+        v.push_back(candidates[j]);
+        find(j,v,candidates,target-candidates[j]);
+        v.pop_back();
+         
+    }
 
 
 }
- 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> v;
         find(0,v,candidates,target);
