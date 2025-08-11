282260adfceb8f57eb9a5118bdc2879aad35d2d6
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-08-11 23:26:27 +0530

    Time: 6 ms (71.27%), Space: 34.9 MB (88.78%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -1,54 +1,40 @@
 class Solution {
 public:
- struct compare {
-    bool operator()(const pair<long long ,int>& p1, const pair<long long ,int>& p2) {
-        return p1.first > p2.first; // Compare based on the first element of the pair
-    }
-};
-    int countPaths(int n, vector<vector<int>>& flights) {
-        // cout<<n;
-        int V=n;
-        vector<pair<int,int>> adj[V];
-        int mod=1e9+7;
-        for(int i=0;i<flights.size();i++){
-            int u=flights[i][0];
-            int v=flights[i][1];
-            int d=flights[i][2];
-            adj[u].push_back({v,d});
-            adj[v].push_back({u,d});
+#define ll long long
+int mod=1e9+7;
+    int countPaths(int n, vector<vector<int>>& times) {
+         vector<pair<int,int>> gr[n+1];
+        for(int i=0;i<times.size();i++){
+            int u=times[i][0];
+            int v=times[i][1];
+            int w=times[i][2];
+            gr[u].push_back({v,w});
+             gr[v].push_back({u,w});
         }
-        priority_queue<pair<long long ,int>,vector<pair<long long ,int>>,compare> pq;
+        priority_queue<pair<ll,int>,vector<pair<ll ,int>>,greater<pair<ll,int>>> pq;
         pq.push({0,0});
-        vector<long long > ways(V,0);
-        vector<long long > dis(V,1e12);
+        vector<int> ways(n+1,0);
         ways[0]=1;
+        vector<ll> dis(n+1,1e15);
         dis[0]=0;
         while(!pq.empty()){
-           long long  d=pq.top().first;
-            int n=pq.top().second;
-           
+            ll d=pq.top().first;
+            int nd=pq.top().second;
             pq.pop();
-           
-            for(auto i:adj[n]){
-                if(dis[i.first]>d+i.second){
-                    ways[i.first]=ways[n];
-                    dis[i.first]=d+i.second;
-                    pq.push({d+i.second,i.first});
+            for(auto i:gr[nd]){
+                int nx=i.first;
+                ll di=i.second;
+                if(d+1ll*di<dis[nx]){
+                    dis[nx]=d+1ll*di;
+                     ways[nx]=ways[nd];
+                    pq.push({dis[nx],nx});
                 }
-                else if(dis[i.first]==d+i.second){
-                         ways[i.first]=(ways[n]+ ways[i.first])%mod;
+                else if(d+1ll*di==dis[nx]){
+                    ways[nx]=(ways[nx]+1ll*ways[nd])%mod;
                 }
             }
         }
-      
-    //   for(auto i:mp[n-1]){
-    //     while(mp[i]){
-
-    //     }
-    //   }
-    // cout<<dis[n-1];
-        // if(dis[n-1]!=1e12&&ways[n-1]==0) return 1;
-        return ways[n-1]%mod;
+        return ways[n-1];
 
     }
 };
