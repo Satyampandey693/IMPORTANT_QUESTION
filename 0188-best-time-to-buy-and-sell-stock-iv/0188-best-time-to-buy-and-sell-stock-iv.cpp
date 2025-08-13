729e666f8578a8852f06e605febc810b0ced879e
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-08-13 14:10:04 +0530

    Time: 15 ms (31.82%), Space: 18.4 MB (17.75%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+int find(int i,vector<int> &prices,int buy,vector<vector<vector<int>>> &dp,int tr){
+    if(tr==0) return 0;
+    if(i==prices.size()) return 0;
+    if(dp[i][buy][tr]!=-1) return dp[i][buy][tr];
+    int profit=INT_MIN;
+    //buy
+    if(buy){
+      profit=max(-prices[i]+find(i+1,prices,0,dp,tr),find(i+1,prices,buy,dp,tr));
+
+    }
+    else{
+        profit=max(prices[i]+find(i+1,prices,1,dp,tr-1),find(i+1,prices,buy,dp,tr));
+    }
+    return dp[i][buy][tr]=profit;
+}
+    int maxProfit(int k, vector<int>& prices) {
+       vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>>(2,vector<int>(k+1,-1)));
+        return find(0,prices,1,dp,k); 
+    }
+};
