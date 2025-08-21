0beaceda94105b9734830cb7a2928e9682ddc5ff
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-08-21 23:42:45 +0530

    Time: 12 ms (51.17%), Space: 24.2 MB (5.03%) - LeetHub

--- 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.cpp
+++ 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
 int find(vector<int> &p){
     int n=p.size();
-//     vector<int> lss(n,n);
-//     int mn=INT_MAX;
-//    int i=n-1;
-//     stack<int> st;
-//    while(i>=0){
-//     while(!st.empty()&&p[st.top()]>p[i]) st.pop();
-//     if(!st.empty()) lss[i]=st.top();
-//     st.push(i);
-//     mn=min(mn,p[i]);
-//     i--;
-//    }
+    vector<int> lss(n,n);
+    int mn=INT_MAX;
+   int i=n-1;
+    stack<int> st;
+   while(i>=0){
+    while(!st.empty()&&p[st.top()]>=p[i]) st.pop();
+    if(!st.empty()) lss[i]=st.top();
+    
+    st.push(i);
+    mn=min(mn,p[i]);
+    i--;
+   }
 //    vector<int> rss(n,-1);
 //    i=0;
 
@@ -25,26 +26,23 @@ int find(vector<int> &p){
 //     i++;
 //    }
    int t=0;
-for(int i=0;i<n;i++){
-    // if(p[i]!=0)
-    // t+=(lss[i]-rss[i])-1 +p[i]-1;
-    // cout<<lss[i]<<" "<<rss[i]<<"nx";
-    int mn=p[i];
-    for(int j=i;j<n;j++){
-        // if()
-        // if(p[j]==0) break;
-        // mn
-
-        t+=min(p[j],mn);
-        mn=min(mn,p[j]);
-    }
+   vector<int> tot(n,0);
+for(int i=n-1;i>=0;i--){
+     
+    // int mn=p[i];
+    // for(int j=i;j<n;j++){
+    //     t+=min(p[j],mn);
+    //     mn=min(mn,p[j]);
+    // }
+    int q=0;
+    int nx=lss[i];
+    q+=(nx-i)*p[i];
+    if(nx<n) q+=tot[nx];
+    tot[i]=q;
+t+=q;
+    
 
 }
-// cout<<mn<<" ";;
-// for(int i=mn-1;i>0;i--){
-//     t+=n*(n+1)/2;
-// }
-// cout<<t<<endl;
 return t;
 
 }
