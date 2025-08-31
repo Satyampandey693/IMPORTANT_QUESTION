ef13d276094190b2d37a825fc0089578760ca3e3
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-08-31 19:44:20 +0530

    Time: 0 ms (100%), Space: 12.5 MB (8.8%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,27 +1,41 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-       int i=m-1;
-       int j=0;
-       while(i>=0&&j<n){
-          if(nums1[i]>nums2[j]){
-            swap(nums1[i],nums2[j]);
-            i--;
-            j++;
+    //    int i=m-1;
+    //    int j=0;
+    //    while(i>=0&&j<n){
+    //       if(nums1[i]>nums2[j]){
+    //         swap(nums1[i],nums2[j]);
+    //         i--;
+    //         j++;
 
-          }
-          else{
-            break;
-          }
-       }
-       sort(nums1.begin(),nums1.begin()+m);
-       sort(nums2.begin(),nums2.end());
-       i=m;
-       j=0;
+    //       }
+    //       else{
+    //         break;
+    //       }
+    //    }
+    //    sort(nums1.begin(),nums1.begin()+m);
+    //    sort(nums2.begin(),nums2.end());
+     int   i=m;
+      int  j=0;
        while(i<n+m){
         nums1[i]=nums2[j];
         i++;
         j++;
        }
+
+    int gap=ceil((n+m)/2.0);
+    while(true){
+    int i=0;
+    int j=gap;
+    while(j<m+n){
+        if(nums1[i]>nums1[j]) swap(nums1[i],nums1[j]);
+        i++;
+        j++;
+    }
+    if(gap==1) break;
+    gap=ceil(gap/2.0);
+    }
+
     }
 };
