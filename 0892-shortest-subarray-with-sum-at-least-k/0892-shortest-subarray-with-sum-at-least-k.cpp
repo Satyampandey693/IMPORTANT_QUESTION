401986f4fa7d30ce75413ef5e00acada5235baed
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-09-03 15:25:44 +0530

    Time: 67 ms (20.6%), Space: 180.7 MB (5.05%) - LeetHub

--- 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
+++ 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
@@ -10,17 +10,31 @@ public:
     }
 
     // Min-heap comparing prefixSum values
-    auto cmp = [&](int a, int b) {
-        return prefixSum[a] > prefixSum[b];
-    };
-    priority_queue<int, vector<int>, decltype(cmp)> minHeap(cmp);
+    // auto cmp = [&](int a, int b) {
+    //     return prefixSum[a] > prefixSum[b];
+    // };
+    // priority_queue<int, vector<int>, decltype(cmp)> minHeap(cmp);
+
+    deque<int> dq;
 
     for (int i = 0; i <= n; i++) {
-        while (!minHeap.empty() && prefixSum[i] - prefixSum[minHeap.top()] >= k) {
-            minLen = min(minLen, i - minHeap.top());
-            minHeap.pop();
+        // while (!minHeap.empty() && prefixSum[i] - prefixSum[minHeap.top()] >= k) {
+        //     minLen = min(minLen, i - minHeap.top());
+        //     minHeap.pop();
+        // }
+        // minHeap.push(i);
+        while(!dq.empty()&&prefixSum[i] - prefixSum[dq.front()] >= k){
+             minLen = min(minLen, i - dq.front());
+           dq.pop_front(); 
         }
-        minHeap.push(i);
+        while(!dq.empty()&&prefixSum[i]<=prefixSum[dq.front()] ) dq.pop_front();
+        // if(!dq.empty()&&prefixSum[i]<=prefixSum[dq.front()])
+        if(dq.empty())dq.push_front(i);
+        else{
+            while(!dq.empty()&&prefixSum[i]<=prefixSum[dq.back()] ) dq.pop_back();
+            dq.push_back(i);
+        }
+
     }
 
     return minLen == INT_MAX ? -1 : minLen;
