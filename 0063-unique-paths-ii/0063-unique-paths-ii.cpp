f2004e728aecf2831dbca969998748aaa59bbda8
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-09-06 15:50:08 +0530

    Time: 0 ms (100%), Space: 11.9 MB (55.06%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+int find(int i,int j,vector<vector<int>>& triangle,  vector<vector<int>> &dp){
+
+   if(i==triangle.size()-1&&j==triangle[0].size()-1) {
+    if(triangle[i][j]==0) return 1;
+    else return 0;
+   }
+    if(i>=triangle.size()){
+        return 0;
+    }
+    
+     if(j>=triangle[i].size()) return 0;
+    if(triangle[i][j]==1) return 0;
+
+    
+     if(dp[i][j]!=-1e8) return dp[i][j];
+    int p=find(i,j+1,triangle,dp);
+    int q=find(i+1,j,triangle,dp);
+    return dp[i][j]=p+q;;
+
+}
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+          vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1e8));
+        return find(0,0,grid,dp);
+    }
+};
