4c6cc2d40505f85535fc3282751c69c000067a13
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-09-06 10:51:03 +0530

    Time: 211 ms (15.14%), Space: 14 MB (29.2%) - LeetHub

---
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+int find(int i,int j,vector<int> &nums, vector<vector<int>> &dp){
+    if(i>j){
+        return 0;
+    }
+    if(dp[i][j]!=-1) return dp[i][j];
+    int mn=0;
+    for(int k=i;k<=j;k++){
+        int p=(nums[k]*nums[j+1]*nums[i-1])+find(i,k-1,nums,dp)+find(k+1,j,nums,dp);
+        mn=max(mn,p);
+    }
+    return dp[i][j]=mn;
+   }
+    int maxCoins(vector<int>& nums) {
+        //  sort(cuts.begin(),cuts.end());
+        vector<int> nc;
+        nc.push_back(1);
+        // nc.insert(cuts.begin(),cuts.end(),nc.end());
+        for(int i=0;i<nums.size();i++) nc.push_back(nums[i]);
+        nc.push_back(1);
+        vector<vector<int>> dp(nums.size()+1,vector<int>(nums.size()+1,-1));
+       return  find(1,nc.size()-2,nc,dp);
+    } 
+};
