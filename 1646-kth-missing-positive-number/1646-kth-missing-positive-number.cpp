3dd0e4ec8b9428254b8608b2f0ebab9f25a03c19
Satyampandey693 <143160965+Satyampandey693@users.noreply.github.com>
2025-09-07 15:03:48 +0530

    Time: 0 ms (100%), Space: 13.2 MB (59.43%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
-    int findKthPositive(vector<int>& piles, int k) {
-        int s = 1;
-        int e = 2000;
-        int ans = -1;
+    int findKthPositive(vector<int>& arr, int k) {
+        // int s = 1;
+        // int e = 2000;
+        // int ans = -1;
 
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
+        // while (s <= e) {
+        //     int mid = s + (e - s) / 2;
 
-            // Count how many numbers are missing <= mid
-            int missing = mid;
-            int actual = upper_bound(piles.begin(), piles.end(), mid) - piles.begin();
-            missing -= actual;
+        //     // Count how many numbers are missing <= mid
+        //     int missing = mid;
+        //     int actual = upper_bound(piles.begin(), piles.end(), mid) - piles.begin();
+        //     missing -= actual;
 
-            if (missing < k) {
-                s = mid + 1;
-            } else {
-                ans = mid; // potential answer
-                e = mid - 1;
+        //     if (missing < k) {
+        //         s = mid + 1;
+        //     } else {
+        //         ans = mid; // potential answer
+        //         e = mid - 1;
+        //     }
+        // }
+        // return ans;
+        if(k<arr[0]) return k;
+        int p=arr[0]-1;
+        int c=p;
+        int ans=0;
+        for(int i=1;i<arr.size();i++){
+            p=arr[i]-arr[i-1]-1;
+            if(c+p>=k){
+                int r=k-c;
+                ans=arr[i-1]+r;
+                c=k+11;
+                break;
             }
+            c+=p;
+        }
+        if(c<=k){
+            ans=arr[arr.size()-1]+(k-c);
         }
         return ans;
+
     }
 };
